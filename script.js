64c35ea26574c85949a32b5cb2f74912debd94c2
muthyalanandini25-crypto <muthyalanandini25@gmail.com>
2026-06-19 12:36:54 +0530

    Refactor login function to use student ID

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 function login(){
 
-    let username =
-    document.getElementById("username").value;
+let id=document.getElementById("studentid").value;
+let pass=document.getElementById("password").value;
 
-    let password =
-    document.getElementById("password").value;
+if(id!=="" && pass!==""){
+alert("Login Successful");
+window.location.href="dashboard.html";
+}
+else{
+alert("Enter Student ID and Password");
+}
 
-    if(username === "Nandini" &&
-       password === "24AFAIM081"){
-
-        document.getElementById("msg")
-        .innerHTML = "Login Successful";
-
-        setTimeout(function(){
-            window.location.href =
-            "dashboard.html";
-        },1000);
-
-    }else{
-
-        document.getElementById("msg")
-        .innerHTML =
-        "Invalid Username or Password";
-    }
 }
