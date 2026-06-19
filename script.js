b4fb41a3c1d65534af1c8c7f1fd9cf245f572bf5
muthyalanandini25-crypto <muthyalanandini25@gmail.com>
2026-06-19 12:21:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ function login(){
     let password =
     document.getElementById("password").value;
 
-    if(username === "student" &&
-       password === "1234"){
+    if(username === "Nandini" &&
+       password === "24AFAIMz081"){
 
         document.getElementById("msg")
         .innerHTML = "Login Successful";
 
+        setTimeout(function(){
+            window.location.href =
+            "dashboard.html";
+        },1000);
+
     }else{
 
         document.getElementById("msg")
-        .innerHTML = "Invalid Username or Password";
+        .innerHTML =
+        "Invalid Username or Password";
     }
 }
