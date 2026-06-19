1a46bc26dcd9d5803ddb56d318f0af2cea5dc92e
muthyalanandini25-crypto <muthyalanandini25@gmail.com>
2026-06-19 13:02:36 +0530

    Update login function for student ID and password

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function login() {
-  let id = document.getElementById("id").value;
-  let pass = document.getElementById("pass").value;
+  let id = document.getElementById("sid").value;
+  let pass = document.getElementById("spass").value;
 
   if (id === "student" && pass === "1234") {
     window.location.href = "dashboard.html";
   } else {
-    document.getElementById("msg").innerText = "Invalid login ❌";
+    document.getElementById("msg").innerText =
+      "Invalid Student ID or Password ❌";
   }
 }
